9e874beee4c375ed6ccb7b87a63401e1b8e08f8d
Akinfemiwa Abayomi Olayode <olayodepossible@gmail.com>
2020-01-29 17:10:58 +0100

    sort line removed

--- src/com/possible/hashcode/Main.java
+++ src/com/possible/hashcode/Main.java
@@ -8,17 +8,18 @@ import java.util.Arrays;
 public class Main {
    private static HashCodePizza configParser;
     public static void main(String[] args)throws FileNotFoundException{
-        int a = 17;
+        int maxNumOfOrder = 17;
         int max = 0;
         int arr [] = {2, 6, 5, 8};
-        int arr1[] = new int[arr.length];
-        System.arraycopy(arr,0,arr1,0, arr.length);
-        Arrays.sort(arr1);
+        int numPizzaType = arr.length;
+        int arr1[] = new int[numPizzaType];
+        System.arraycopy(arr,0,arr1,0, numPizzaType);
+//        Arrays.sort(arr1);
 
 
-        for(int i = arr1.length -1 ; i >= 0; i--){
-            if(arr1[i] < a){
-                a = a - arr1[i];
+        for(int i = numPizzaType -1 ; i >= 0; i--){
+            if(arr1[i] < maxNumOfOrder){
+                maxNumOfOrder = maxNumOfOrder - arr1[i];
                 System.out.printf("%d%s",i," ");
                 max++;
             }
