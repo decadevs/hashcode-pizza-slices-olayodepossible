787a0554c897b7af570bb8ceeb1aa77a2fae32d7
Akinfemiwa Abayomi Olayode <olayodepossible@gmail.com>
2020-01-31 12:19:23 +0100

    class sepration

--- src/com/possible/hashcode/HashCodePizza.java
+++ src/com/possible/hashcode/HashCodePizza.java
@@ -1,7 +1,81 @@
 package com.possible.hashcode;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HashCodePizza {
+    private Path p;
+    private int[] pizzaSlice;
+    private int pizzaType;
+    private int numOfParticipant;
+    private int maxOrder;
 
+    public HashCodePizza(String fileName){
+        p = Paths.get(fileName).toAbsolutePath();
+        getPizzaSlice(p);
+
+    }
+
+    private void getPizzaSlice(Path p){
+        int pizzaSliceArray[] = new int[0];
+        try (BufferedReader inputVal = new BufferedReader(new FileReader(String.valueOf(p)))){
+            String eachLine = "";
+            String [] lines  = new String [2];
+            int counter = 0;
+            while((eachLine = inputVal.readLine()) != null){
+                lines[counter] = eachLine;
+                counter++;
+            }
+
+            String [] lineOneSplit = lines[0].split(" ");
+            numOfParticipant = Integer.parseInt(lineOneSplit[0]);
+            pizzaType = Integer.parseInt(lineOneSplit[1]);
+            pizzaSliceArray = new int [pizzaType];
+
+            String [] lineTwoSplit = lines[1].split(" ");
+            // int [] pizzaSliceArray = new int [eachLineSplit.length];
+            for(int i = 0; i < lineTwoSplit.length; i++){
+                pizzaSliceArray[i] = Integer.parseInt(lineTwoSplit[i]);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        orderedPizza(pizzaSliceArray);
+    }
+
+    private void orderedPizza(int [] pizzaSlice){
+        try(PrintWriter writeOrder = new PrintWriter(new FileWriter("./resultFile.txt"))){
+            List<String> orderedSlice = new ArrayList<>();
+            int orderedSliceIndex = pizzaType - 1;
+            for(int index = orderedSliceIndex ; index >= 0; index--){
+                int numOfPizzaSlice = pizzaSlice[index];
+                if(numOfPizzaSlice < numOfParticipant){
+                    numOfParticipant = numOfParticipant - numOfPizzaSlice;
+                    orderedSlice.add(String.valueOf(index));
+                    maxOrder++;
+                }
+            }
+
+            Collections.reverse(orderedSlice);
+            String[] tempsArray =  orderedSlice.toArray(new String[0]);
+            writeOrder.println(maxOrder);
+
+            for(String orderedIndex : tempsArray){
+                writeOrder.printf("%s%s",orderedIndex," ");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- src/com/possible/hashcode/Main.java
+++ src/com/possible/hashcode/Main.java
@@ -1,30 +1,13 @@
 package com.possible.hashcode;
 
 
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
-   private static HashCodePizza configParser;
-    public static void main(String[] args)throws FileNotFoundException{
-        int maxNumOfOrder = 17;
-        int max = 0;
-        int arr [] = {2, 5, 6, 8};
-        int numPizzaType = arr.length;
-        int arr1[] = new int[numPizzaType];
-        System.arraycopy(arr,0,arr1,0, numPizzaType);
-//        Arrays.sort(arr1);
+    private static HashCodePizza configParser;
+    public static void main(String[] args){
 
-
-        for(int i = numPizzaType -1 ; i >= 0; i--){
-            if(arr1[i] < maxNumOfOrder){
-                maxNumOfOrder = maxNumOfOrder - arr1[i];
-                System.out.printf("%d%s",i," ");
-                max++;
-            }
-        }
-        System.out.println();
-        System.out.println(max);
+       String fileName = "hashcode.txt";
+       configParser = new HashCodePizza(fileName);
     }
 }
